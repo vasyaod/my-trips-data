47ae93848da7ebb60f7016bb1afdf5c76a1615b8
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2022-04-09 18:48:01 -0700

    Push

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    /**
+     * The url where data model is, it could be change for debug purpose.
+     * 
+     * For example: https://vasyaod.github.io/my-tracks/
+     */
+    url: "./",
+
+    /**
+     * Caption of the site.
+     */
+    siteTitle: "Favorite Bike Tracks",
+
+    /**
+     * Site description on the top of index page
+     */
+    siteDescription: "Collection of my favorite bike and walking tracks"
+}
